4ffa85ffdbad37adba7acf622bde6ab007e59cfc
OxoTHuKxD <oxothukxd76@gmail.com>
2016-12-22 18:34:52 +0300

    Задача: Доработать личный кабинет неавторизованного пользователя.(https://bureauit.bitrix24.ru/company/personal/user/184/tasks/task/view/2834/)

--- protected/views/cabinet/_user_title.php
+++ protected/views/cabinet/_user_title.php
@@ -1,20 +1,26 @@
 <div class="user__title">
-
-
-
-    <? if(Yii::app()->user->isGuest || $this->checkUserIndeed()): ?>
-    <a href="/cabinet/index" class="user__title-btn <?= ($this->action->Id == 'index') ? 'active' : '' ?>">
-        Мои объявления
-    </a>
-	<? endif ?>
+    <p>Вы вошли в личный кабинет как не авторизованный пользователь, чтобы воспользоваться всеми функциями авторизируйтесь или зарегистрируйтесь.</p>
+    <?php if(!Yii::app()->user->isGuest || $this->checkUserIndeed()){ ?>
+        <a href="/cabinet/index" class="user__title-btn <?= ($this->action->Id == 'index') ? 'active' : '' ?>">
+            Мои объявления
+        </a>
+	<?php } else {?>
+        <a href="#" style="color: gray; cursor: text; border: none;" onclick="return false;" class="user__title-btn">
+            Мои объявления
+        </a>
+    <?php }?>
 
     <a href="/cabinet/favorites" class="user__title-btn <?= ($this->action->Id == 'favorites') ? 'active' : '' ?>">
         Избранное
     </a>
-    
-    <? if(Yii::app()->user->isGuest || $this->checkUserIndeed()): ?>
-    <a href="/cabinet/personal" class="user__title-btn <?= ($this->action->Id == 'changepassword' || $this->action->Id == 'personal') ? 'active' : '' ?>">
-        Персональные данные
-    </a>
-	<? endif ?>
+
+    <?php if(!Yii::app()->user->isGuest || $this->checkUserIndeed()){ ?>
+        <a href="/cabinet/personal" class="user__title-btn <?= ($this->action->Id == 'changepassword' || $this->action->Id == 'personal') ? 'active' : '' ?>">
+            Персональные данные
+        </a>
+    <?php } else {?>
+        <a href="#" style="color: gray; cursor: text; border: none;" onclick="return false;" class="user__title-btn">
+            Персональные данные
+        </a>
+    <?php }?>
 </div>
